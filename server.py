3498396c0295bf13f77c14adbc10b3e653d5889d
lukas madon <lukas@firestartr.co>
2014-10-04 22:27:48 +0100

    set up

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return 'Hello World!'
+	if request.json and request.json.get("answer") == os.getenv("ANSWER"):
+		return os.getenv("JOB_EMAIL") + "\n"
+	return "%s %s" % (os.getenv("QUESTION"),"POST the json to server with answer -> { 'answer': 'xxx'}")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
