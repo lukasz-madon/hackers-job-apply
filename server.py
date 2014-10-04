9278cec936df76de40dfedd632ce5d20cb653491
lukas madon <lukas@firestartr.co>
2014-10-04 22:35:15 +0100

    typos

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
 	if request.json and request.json.get("answer") == os.getenv("ANSWER"):
 		return os.getenv("JOB_EMAIL") + "\n"
-	return "%s %s" % (os.getenv("QUESTION"),"POST the json to server with answer -> { 'answer': 'xxx'}")
+	return "%s %s" % (os.getenv("QUESTION"), "POST json to the server with the answer -> { 'answer': 'xxx'}\n")
 
 
 if __name__ == "__main__":
